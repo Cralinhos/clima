be832725f8125b1f184c3c3750ce02cf95e32ffa
Cralinhos <carlosaugustocostadesousa6@gmail.com>
2024-10-22 19:15:09 -0300

    melhoria na pagina
    apenas dados importantes

--- script.js
+++ script.js
@@ -1,92 +1,48 @@
-const apiKey = "3e4eb591a2ee5dcaf4bfb2d517413ca3";
-const apiCountryURL = "https://countryflagsapi.com/png/";
-const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+const chaveAPI = "3e4eb591a2ee5dcaf4bfb2d517413ca3";
+const cidadeFixa = "São João dos Patos";
 
-const cityInput = document.querySelector("#city-input");
-const searchBtn = document.querySelector("#search");
+const elementoTemperatura = document.querySelector("#temperatura span");
+const elementoUmidade = document.querySelector("#umidade span");
+const carregando = document.querySelector("#carregando");
 
-const cityElement = document.querySelector("#city");
-const tempElement = document.querySelector("#temperature span");
-const descElement = document.querySelector("#description");
-const weatherIconElement = document.querySelector("#weather-icon");
-const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
-const windElement = document.querySelector("#wind span");
-
-const weatherContainer = document.querySelector("#weather-data");
-
-const errorMessageContainer = document.querySelector("#error-message");
-const loader = document.querySelector("#loader");
-
-const suggestionContainer = document.querySelector("#suggestions");
-const suggestionButtons = document.querySelectorAll("#suggestions button");
-
-// Loader
-const toggleLoader = () => {
-  loader.classList.toggle("hide");
+// Função para mostrar/ocultar o loader
+const alternarCarregando = () => {
+  carregando.classList.toggle("esconder");
 };
 
-const getWeatherData = async (city) => {
-  toggleLoader();
+// Função para obter os dados do clima
+const obterDadosClima = async (cidade) => {
+  alternarCarregando();
+  
+  const apiURLClima = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${chaveAPI}&lang=pt_br`;
+  
+  const resposta = await fetch(apiURLClima);
+  const dados = await resposta.json();
 
-  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-
-  const res = await fetch(apiWeatherURL);
-  const data = await res.json();
-
-  toggleLoader();
-
-  return data;
+  alternarCarregando();
+  
+  return dados;
 };
 
-// Tratamento de erro
-const showErrorMessage = () => {
-  errorMessageContainer.classList.remove("hide");
-};
+// Função para exibir os dados do clima
+const mostrarDadosClima = async (cidade) => {
+  const dados = await obterDadosClima(cidade);
 
-const hideInformation = () => {
-  errorMessageContainer.classList.add("hide");
-  weatherContainer.classList.add("hide");
-
-  suggestionContainer.classList.add("hide");
-};
-
-const showWeatherData = async (city) => {
-  hideInformation();
-
-  const data = await getWeatherData(city);
-
-  if (data.cod === "404") {
-    showErrorMessage();
+  if (dados.cod === "404") {
+    alert("Erro: Cidade não encontrada");
     return;
   }
 
-  cityElement.innerText = data.name;
-  tempElement.innerText = parseInt(data.main.temp);
-  descElement.innerText = data.weather[0].description;
-  weatherIconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-  );
-  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
-  umidityElement.innerText = `${data.main.humidity}%`;
-  windElement.innerText = `${data.wind.speed}km/h`;
-
-  weatherContainer.classList.remove("hide");
+  elementoTemperatura.innerText = parseInt(dados.main.temp);
+  elementoUmidade.innerText = `${dados.main.humidity}`;
 };
 
-searchBtn.addEventListener("click", async (e) => {
-  e.preventDefault();
+// Atualiza os dados de São João dos Patos ao carregar a página
+window.addEventListener("load", () => {
+  mostrarDadosClima(cidadeFixa);
 
-  const city = cityInput.value;
-
-  showWeatherData(city);
-});
-
-cityInput.addEventListener("keyup", (e) => {
-  if (e.code === "Enter") {
-    const city = e.target.value;
-
-    showWeatherData(city);
-  }
+  // Atualizar a cada 10 minutos (600.000 milissegundos)
+  setInterval(() => {
+    mostrarDadosClima(cidadeFixa);
+  }, 300000);
 });
